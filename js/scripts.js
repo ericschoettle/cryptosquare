3cb5fb61ca6cecac6af4f12d5ea49e4147074ca9
Eric Schoettle and Max Reichman <eric+max@>
2017-03-30 16:54:58 -0700

    add decryption. Needs refactoring.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ function Cryptosquare(string) {
   var lowerCase = dePunctuate.replace(/\s/g,"").toLowerCase().split("");
   var arraySize = Math.ceil(Math.sqrt(lowerCase.length));
   var squareArray = [];
-  for (var i = 0; i < lowerCase.length; i+=arraySize) {
-    squareArray.push(lowerCase.slice(i, i+arraySize));
+  for (var i = 0; i < lowerCase.length; i += arraySize) {
+    squareArray.push(lowerCase.slice(i, i + arraySize));
   }
   var cryptoString = "";
   for (var i = 0; i < arraySize; i++) {
@@ -21,4 +21,51 @@ function Cryptosquare(string) {
   return cryptoStringDivided;
 }
 
-console.log(Cryptosquare("this is fat abc"));
+console.log(Cryptosquare("this is fat or"));
+
+
+function DecryptSquare(string) {
+  var noSpaces = string.replace(/\s/g,"").split("");
+  var arrayWidth = Math.ceil(Math.sqrt(noSpaces.length));
+  console.log(arrayWidth)
+  var remainder = arrayWidth * arrayWidth - noSpaces.length;
+  console.log(remainder)
+  if (remainder > arrayWidth) {
+    var arrayHeight = arrayWidth - 1;
+    remainder = remainder - arrayWidth;
+  } else {
+    var arrayHeight = arrayWidth;
+  }
+  var boxOneWidth = arrayWidth-remainder;
+  var boxOneHeight = arrayHeight;
+  var boxTwoWidth = remainder;
+  var boxTwoHeight = arrayHeight - 1;
+  console.log(boxOneHeight)
+  console.log(boxOneWidth)
+  // make boxes
+  var boxOne = [];
+  for (var i = 0; i < boxOneWidth; i++) {
+    boxOne.push(noSpaces.slice(i * boxOneHeight, (i + 1) * boxOneHeight));
+  } console.log(boxOne)
+
+  for (var i = 0; i < boxTwoWidth; i++) {
+    boxOne.push(noSpaces.slice(i * boxTwoHeight + boxOneHeight*boxOneWidth, (i + 1) * boxTwoHeight + boxOneHeight*boxOneWidth));
+  }
+
+
+  // readBoxes
+  var cryptoString = "";
+  for (var i = 0; i < arrayWidth; i++) {
+    for (var j = 0; j < arrayWidth; j++) {
+      debugger
+      if (boxOne[j][i]) {
+        cryptoString = cryptoString + boxOne[j][i];
+      }
+    }
+  }
+
+  console.log(cryptoString)
+}
+
+
+console.log(DecryptSquare("tiths oifrs a"))
